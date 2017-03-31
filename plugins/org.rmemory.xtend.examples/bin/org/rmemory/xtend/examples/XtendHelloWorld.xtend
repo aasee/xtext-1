d8cf50ee1fe9064f3984900c09ed465348c43196
Richard Memory <richardfmemory@gmail.com>
2017-03-31 15:47:32 -0600

    Add hello world xtend example

---
+++ plugins/org.rmemory.xtend.examples/bin/org/rmemory/xtend/examples/XtendHelloWorld.xtend
@@ -0,0 +1,7 @@
+package org.rmemory.xtend.examples
+
+class XtendHelloWorld {
+	def static void main(String[] args) {
+		println("Hello world")
+	}
+}
---
+++ plugins/org.rmemory.xtend.examples/xtend-gen/org/rmemory/xtend/examples/XtendHelloWorld.java
@@ -0,0 +1,10 @@
+package org.rmemory.xtend.examples;
+
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+@SuppressWarnings("all")
+public class XtendHelloWorld {
+  public static void main(final String[] args) {
+    InputOutput.<String>println("Hello world");
+  }
+}
